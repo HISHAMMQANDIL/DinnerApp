931fa897077f56ab42cccc4c84d754927ca7a027
hisham.qandil@gmail.com <hisham.qandil@gmail.com>
2020-09-02 18:04:51 -0700

    All task

--- app/src/main/java/com/example/dinnerapp/MainActivity.kt
+++ app/src/main/java/com/example/dinnerapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.dinnerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        decide.setOnClickListener {
+// Change the Label Text
+            food.text = " ${list.random()}!"
+
+        }
+
+        addfood.setOnClickListener{
+            val element = newfood.text
+            list.add(element.toString())
+        }
+
     }
+    val list = arrayListOf( "Hamburger", "Pizza",
+        "Mexican", "American", "Chinese")
+
+
+
+
 }
